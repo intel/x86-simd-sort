0b62b1bbe955b2c218efd5c10237ea01e882ce5c
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2023-03-27 16:04:23 -0700

    Add runtime check to the keyvalue test

--- tests/test_keyvalue.cpp
+++ tests/test_keyvalue.cpp
@@ -5,6 +5,7 @@
 
 #include "avx512-64bit-keyvaluesort.hpp"
 #include "rand_array.h"
+#include "cpuinfo.h"
 #include <gtest/gtest.h>
 #include <vector>
 
@@ -28,37 +29,42 @@ TYPED_TEST_SUITE_P(TestKeyValueSort);
 
 TYPED_TEST_P(TestKeyValueSort, KeyValueSort)
 {
-    std::vector<int64_t> keysizes;
-    for (int64_t ii = 0; ii < 1024; ++ii) {
-        keysizes.push_back((TypeParam)ii);
-    }
-    std::vector<TypeParam> keys;
-    std::vector<uint64_t> values;
-    std::vector<sorted_t<TypeParam, uint64_t>> sortedarr;
+    if (cpu_has_avx512bw()) {
+        std::vector<int64_t> keysizes;
+        for (int64_t ii = 0; ii < 1024; ++ii) {
+            keysizes.push_back((TypeParam)ii);
+        }
+        std::vector<TypeParam> keys;
+        std::vector<uint64_t> values;
+        std::vector<sorted_t<TypeParam, uint64_t>> sortedarr;
 
-    for (size_t ii = 0; ii < keysizes.size(); ++ii) {
-        /* Random array */
-        keys = get_uniform_rand_array_with_uniquevalues<TypeParam>(
-                keysizes[ii]);
-        values = get_uniform_rand_array<uint64_t>(keysizes[ii]);
-        for (size_t i = 0; i < keys.size(); i++) {
-            sorted_t<TypeParam, uint64_t> tmp_s;
-            tmp_s.key = keys[i];
-            tmp_s.value = values[i];
-            sortedarr.emplace_back(tmp_s);
+        for (size_t ii = 0; ii < keysizes.size(); ++ii) {
+            /* Random array */
+            keys = get_uniform_rand_array_with_uniquevalues<TypeParam>(
+                    keysizes[ii]);
+            values = get_uniform_rand_array<uint64_t>(keysizes[ii]);
+            for (size_t i = 0; i < keys.size(); i++) {
+                sorted_t<TypeParam, uint64_t> tmp_s;
+                tmp_s.key = keys[i];
+                tmp_s.value = values[i];
+                sortedarr.emplace_back(tmp_s);
+            }
+            /* Sort with std::sort for comparison */
+            std::sort(sortedarr.begin(),
+                      sortedarr.end(),
+                      compare<TypeParam, uint64_t>);
+            avx512_qsort_kv<TypeParam>(keys.data(), values.data(), keys.size());
+            for (size_t i = 0; i < keys.size(); i++) {
+                ASSERT_EQ(keys[i], sortedarr[i].key);
+                ASSERT_EQ(values[i], sortedarr[i].value);
+            }
+            keys.clear();
+            values.clear();
+            sortedarr.clear();
         }
-        /* Sort with std::sort for comparison */
-        std::sort(sortedarr.begin(),
-                  sortedarr.end(),
-                  compare<TypeParam, uint64_t>);
-        avx512_qsort_kv<TypeParam>(keys.data(), values.data(), keys.size());
-        for (size_t i = 0; i < keys.size(); i++) {
-            ASSERT_EQ(keys[i], sortedarr[i].key);
-            ASSERT_EQ(values[i], sortedarr[i].value);
-        }
-        keys.clear();
-        values.clear();
-        sortedarr.clear();
+    }
+    else {
+        GTEST_SKIP() << "Skipping this test, it requires avx512bw";
     }
 }
 
