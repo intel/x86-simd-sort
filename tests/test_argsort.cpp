c4bb73485754d11beed5e9c58c886800f4cfb180
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2023-05-01 12:41:25 -0700

    Add tests for uint64_t and double argsort

--- tests/test_argsort.cpp
+++ tests/test_argsort.cpp
@@ -11,6 +11,11 @@
 #include <vector>
 
 template <typename T>
+class avx512argsort : public ::testing::Test {
+};
+TYPED_TEST_SUITE_P(avx512argsort);
+
+template <typename T>
 std::vector<int64_t> std_argsort(const std::vector<T> &array)
 {
     std::vector<int64_t> indices(array.size());
@@ -25,21 +30,21 @@ std::vector<int64_t> std_argsort(const std::vector<T> &array)
     return indices;
 }
 
-TEST(avx512_argsort_64bit, test_random)
+TYPED_TEST_P(avx512argsort, test_random)
 {
     if (cpu_has_avx512bw()) {
         std::vector<int64_t> arrsizes;
         for (int64_t ii = 0; ii <= 1024; ++ii) {
             arrsizes.push_back(ii);
         }
-        std::vector<int64_t> arr;
+        std::vector<TypeParam> arr;
         for (auto &size : arrsizes) {
             /* Random array */
-            arr = get_uniform_rand_array<int64_t>(size);
+            arr = get_uniform_rand_array<TypeParam>(size);
             std::vector<int64_t> inx1 = std_argsort(arr);
             std::vector<int64_t> inx2
-                    = avx512_argsort<int64_t>(arr.data(), arr.size());
-            std::vector<int64_t> sort1, sort2;
+                    = avx512_argsort<TypeParam>(arr.data(), arr.size());
+            std::vector<TypeParam> sort1, sort2;
             for (size_t jj = 0; jj < size; ++jj) {
                 sort1.push_back(arr[inx1[jj]]);
                 sort2.push_back(arr[inx2[jj]]);
@@ -53,24 +58,24 @@ TEST(avx512_argsort_64bit, test_random)
     }
 }
 
-TEST(avx512_argsort_64bit, test_constant)
+TYPED_TEST_P(avx512argsort, test_constant)
 {
     if (cpu_has_avx512bw()) {
         std::vector<int64_t> arrsizes;
         for (int64_t ii = 0; ii <= 1024; ++ii) {
             arrsizes.push_back(ii);
         }
-        std::vector<int64_t> arr;
+        std::vector<TypeParam> arr;
         for (auto &size : arrsizes) {
             /* constant array */
-            auto elem = get_uniform_rand_array<int64_t>(1)[0];
+            auto elem = get_uniform_rand_array<TypeParam>(1)[0];
             for (int64_t jj = 0; jj < size; ++jj) {
                 arr.push_back(elem);
             }
             std::vector<int64_t> inx1 = std_argsort(arr);
             std::vector<int64_t> inx2
-                    = avx512_argsort<int64_t>(arr.data(), arr.size());
-            std::vector<int64_t> sort1, sort2;
+                    = avx512_argsort<TypeParam>(arr.data(), arr.size());
+            std::vector<TypeParam> sort1, sort2;
             for (size_t jj = 0; jj < size; ++jj) {
                 sort1.push_back(arr[inx1[jj]]);
                 sort2.push_back(arr[inx2[jj]]);
@@ -84,21 +89,21 @@ TEST(avx512_argsort_64bit, test_constant)
     }
 }
 
-TEST(avx512_argsort_64bit, test_small_range)
+TYPED_TEST_P(avx512argsort, test_small_range)
 {
     if (cpu_has_avx512bw()) {
         std::vector<int64_t> arrsizes;
         for (int64_t ii = 0; ii <= 1024; ++ii) {
             arrsizes.push_back(ii);
         }
-        std::vector<int64_t> arr;
+        std::vector<TypeParam> arr;
         for (auto &size : arrsizes) {
             /* array with a smaller range of values */
-            arr = get_uniform_rand_array<int64_t>(size, 20, 1);
+            arr = get_uniform_rand_array<TypeParam>(size, 20, 1);
             std::vector<int64_t> inx1 = std_argsort(arr);
             std::vector<int64_t> inx2
-                    = avx512_argsort<int64_t>(arr.data(), arr.size());
-            std::vector<int64_t> sort1, sort2;
+                    = avx512_argsort<TypeParam>(arr.data(), arr.size());
+            std::vector<TypeParam> sort1, sort2;
             for (size_t jj = 0; jj < size; ++jj) {
                 sort1.push_back(arr[inx1[jj]]);
                 sort2.push_back(arr[inx2[jj]]);
@@ -112,21 +117,21 @@ TEST(avx512_argsort_64bit, test_small_range)
     }
 }
 
-TEST(avx512_argsort_64bit, test_sorted)
+TYPED_TEST_P(avx512argsort, test_sorted)
 {
     if (cpu_has_avx512bw()) {
         std::vector<int64_t> arrsizes;
         for (int64_t ii = 0; ii <= 1024; ++ii) {
             arrsizes.push_back(ii);
         }
-        std::vector<int64_t> arr;
+        std::vector<TypeParam> arr;
         for (auto &size : arrsizes) {
-            arr = get_uniform_rand_array<int64_t>(size);
+            arr = get_uniform_rand_array<TypeParam>(size);
             std::sort(arr.begin(), arr.end());
             std::vector<int64_t> inx1 = std_argsort(arr);
             std::vector<int64_t> inx2
-                    = avx512_argsort<int64_t>(arr.data(), arr.size());
-            std::vector<int64_t> sort1, sort2;
+                    = avx512_argsort<TypeParam>(arr.data(), arr.size());
+            std::vector<TypeParam> sort1, sort2;
             for (size_t jj = 0; jj < size; ++jj) {
                 sort1.push_back(arr[inx1[jj]]);
                 sort2.push_back(arr[inx2[jj]]);
@@ -140,22 +145,22 @@ TEST(avx512_argsort_64bit, test_sorted)
     }
 }
 
-TEST(avx512_argsort_64bit, test_reverse)
+TYPED_TEST_P(avx512argsort, test_reverse)
 {
     if (cpu_has_avx512bw()) {
         std::vector<int64_t> arrsizes;
         for (int64_t ii = 0; ii <= 1024; ++ii) {
             arrsizes.push_back(ii);
         }
-        std::vector<int64_t> arr;
+        std::vector<TypeParam> arr;
         for (auto &size : arrsizes) {
-            arr = get_uniform_rand_array<int64_t>(size);
+            arr = get_uniform_rand_array<TypeParam>(size);
             std::sort(arr.begin(), arr.end());
             std::reverse(arr.begin(), arr.end());
             std::vector<int64_t> inx1 = std_argsort(arr);
             std::vector<int64_t> inx2
-                    = avx512_argsort<int64_t>(arr.data(), arr.size());
-            std::vector<int64_t> sort1, sort2;
+                    = avx512_argsort<TypeParam>(arr.data(), arr.size());
+            std::vector<TypeParam> sort1, sort2;
             for (size_t jj = 0; jj < size; ++jj) {
                 sort1.push_back(arr[inx1[jj]]);
                 sort2.push_back(arr[inx2[jj]]);
@@ -168,3 +173,16 @@ TEST(avx512_argsort_64bit, test_reverse)
         GTEST_SKIP() << "Skipping this test, it requires avx512bw ISA";
     }
 }
+
+REGISTER_TYPED_TEST_SUITE_P(avx512argsort,
+                            test_random,
+                            test_reverse,
+                            test_constant,
+                            test_sorted,
+                            test_small_range);
+
+using ArgSortTestTypes = testing::Types<uint64_t,
+                                        int64_t,
+                                        double>;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(TestPrefix, avx512argsort, ArgSortTestTypes);
