63c2bf3e5ae753ee798442975a4e6c1488522872
Matthew Sterrett <matthew.sterrett@intel.com>
2023-07-17 11:21:28 -0700

    Basic initial benchmark and test

---
+++ benchmarks/bench-avx2.cpp
@@ -0,0 +1,124 @@
+#include "avx2-32bit-qsort.hpp"
+#include "cpuinfo.h"
+#include "rand_array.h"
+#include <benchmark/benchmark.h>
+
+#define MY_BENCHMARK_CAPTURE(func, T, test_case_name, ...) \
+    BENCHMARK_PRIVATE_DECLARE(func) \
+            = (::benchmark::internal::RegisterBenchmarkInternal( \
+                    new ::benchmark::internal::FunctionBenchmark( \
+                            #func "/" #test_case_name "/" #T, \
+                            [](::benchmark::State &st) { \
+                                func<T>(st, __VA_ARGS__); \
+                            })))
+
+#define BENCH(func, type) \
+    MY_BENCHMARK_CAPTURE( \
+            func, type, random_5k, 5000, std::string("random")); \
+    MY_BENCHMARK_CAPTURE( \
+            func, type, random_100k, 100000, std::string("random")); \
+    MY_BENCHMARK_CAPTURE( \
+            func, type, random_1m, 1000000, std::string("random")); \
+    MY_BENCHMARK_CAPTURE( \
+            func, type, random_10m, 10000000, std::string("random")); \
+    MY_BENCHMARK_CAPTURE( \
+            func, type, sorted_10k, 10000, std::string("sorted")); \
+    MY_BENCHMARK_CAPTURE( \
+            func, type, constant_10k, 10000, std::string("constant")); \
+    MY_BENCHMARK_CAPTURE( \
+            func, type, reverse_10k, 10000, std::string("reverse"));
+
+template <typename T, class... Args>
+static void stdsort(benchmark::State &state, Args &&...args)
+{
+    auto args_tuple = std::make_tuple(std::move(args)...);
+    // Perform setup here
+    size_t ARRSIZE = std::get<0>(args_tuple);
+    std::vector<T> arr;
+    std::vector<T> arr_bkp;
+
+    std::string arrtype = std::get<1>(args_tuple);
+    if (arrtype == "random") { arr = get_uniform_rand_array<T>(ARRSIZE); }
+    else if (arrtype == "sorted") {
+        arr = get_uniform_rand_array<T>(ARRSIZE);
+        std::sort(arr.begin(), arr.end());
+    }
+    else if (arrtype == "constant") {
+        T temp = get_uniform_rand_array<T>(1)[0];
+        for (size_t ii = 0; ii < ARRSIZE; ++ii) {
+            arr.push_back(temp);
+        }
+    }
+    else if (arrtype == "reverse") {
+        arr = get_uniform_rand_array<T>(ARRSIZE);
+        std::sort(arr.begin(), arr.end());
+        std::reverse(arr.begin(), arr.end());
+    }
+    arr_bkp = arr;
+
+    /* call avx512 quicksort */
+    for (auto _ : state) {
+        std::sort(arr.begin(), arr.end());
+        state.PauseTiming();
+        arr = arr_bkp;
+        state.ResumeTiming();
+    }
+}
+
+template <typename T, class... Args>
+static void avx2qsort(benchmark::State &state, Args &&...args)
+{
+    auto args_tuple = std::make_tuple(std::move(args)...);
+    if (!cpu_has_avx512bw()) {
+        state.SkipWithMessage("Requires AVX512 BW ISA");
+    }
+    if ((sizeof(T) == 2) && (!cpu_has_avx512_vbmi2())) {
+        state.SkipWithMessage("Requires AVX512 VBMI2");
+    }
+    // Perform setup here
+    size_t ARRSIZE = std::get<0>(args_tuple);
+    std::vector<T> arr;
+    std::vector<T> arr_bkp;
+
+    std::string arrtype = std::get<1>(args_tuple);
+    if (arrtype == "random") { arr = get_uniform_rand_array<T>(ARRSIZE); }
+    else if (arrtype == "sorted") {
+        arr = get_uniform_rand_array<T>(ARRSIZE);
+        std::sort(arr.begin(), arr.end());
+    }
+    else if (arrtype == "constant") {
+        T temp = get_uniform_rand_array<T>(1)[0];
+        for (size_t ii = 0; ii < ARRSIZE; ++ii) {
+            arr.push_back(temp);
+        }
+    }
+    else if (arrtype == "reverse") {
+        arr = get_uniform_rand_array<T>(ARRSIZE);
+        std::sort(arr.begin(), arr.end());
+        std::reverse(arr.begin(), arr.end());
+    }
+    arr_bkp = arr;
+
+    /* call avx512 quicksort */
+    for (auto _ : state) {
+        avx2_qsort<T>(arr.data(), ARRSIZE);
+        state.PauseTiming();
+        arr = arr_bkp;
+        state.ResumeTiming();
+    }
+}
+
+#define BENCH_BOTH_QSORT(type)\
+    BENCH(avx2qsort, type)\
+    BENCH(stdsort, type)
+
+//BENCH_BOTH_QSORT(uint64_t)
+//BENCH_BOTH_QSORT(int64_t)
+BENCH_BOTH_QSORT(uint32_t)
+BENCH_BOTH_QSORT(int32_t)
+//BENCH_BOTH_QSORT(uint16_t)
+//BENCH_BOTH_QSORT(int16_t)
+BENCH_BOTH_QSORT(float)
+//BENCH_BOTH_QSORT(double)
+
+
---
+++ tests/test-avx2.cpp
@@ -0,0 +1,116 @@
+#include "avx2-32bit-qsort.hpp"
+#include "cpuinfo.h"
+#include "rand_array.h"
+#include <gtest/gtest.h>
+
+template <typename T>
+class avx2_sort : public ::testing::Test {
+};
+TYPED_TEST_SUITE_P(avx2_sort);
+
+TYPED_TEST_P(avx2_sort, test_random)
+{
+    std::vector<int64_t> arrsizes;
+    for (int64_t ii = 0; ii < 1024; ++ii) {
+        arrsizes.push_back((TypeParam)ii);
+    }
+    std::vector<TypeParam> arr;
+    std::vector<TypeParam> sortedarr;
+    for (size_t ii = 0; ii < arrsizes.size(); ++ii) {
+        /* Random array */
+        arr = get_uniform_rand_array<TypeParam>(arrsizes[ii]);
+        sortedarr = arr;
+        /* Sort with std::sort for comparison */
+        std::sort(sortedarr.begin(), sortedarr.end());
+        avx2_qsort<TypeParam>(arr.data(), arr.size());
+        ASSERT_EQ(sortedarr, arr) << "Array size = " << arrsizes[ii];
+        arr.clear();
+        sortedarr.clear();
+    }
+}
+
+REGISTER_TYPED_TEST_SUITE_P(avx2_sort,
+                            test_random);
+
+using Types = testing::Types<float, int32_t, uint32_t>;
+INSTANTIATE_TYPED_TEST_SUITE_P(T, avx2_sort, Types);
+
+template <typename T>
+class avx2_select : public ::testing::Test {
+};
+TYPED_TEST_SUITE_P(avx2_select);
+
+TYPED_TEST_P(avx2_select, test_random)
+{
+    std::vector<int64_t> arrsizes;
+    for (int64_t ii = 0; ii < 1024; ++ii) {
+        arrsizes.push_back(ii);
+    }
+    std::vector<TypeParam> arr;
+    std::vector<TypeParam> sortedarr;
+    std::vector<TypeParam> psortedarr;
+    for (size_t ii = 0; ii < arrsizes.size(); ++ii) {
+        /* Random array */
+        arr = get_uniform_rand_array<TypeParam>(arrsizes[ii]);
+        sortedarr = arr;
+        /* Sort with std::sort for comparison */
+        std::sort(sortedarr.begin(), sortedarr.end());
+        for (size_t k = 0; k < arr.size(); ++k) {
+            psortedarr = arr;
+            avx2_qselect<TypeParam>(
+                    psortedarr.data(), k, psortedarr.size());
+            /* index k is correct */
+            ASSERT_EQ(sortedarr[k], psortedarr[k]);
+            /* Check left partition */
+            for (size_t jj = 0; jj < k; jj++) {
+                ASSERT_LE(psortedarr[jj], psortedarr[k]);
+            }
+            /* Check right partition */
+            for (size_t jj = k + 1; jj < arr.size(); jj++) {
+                ASSERT_GE(psortedarr[jj], psortedarr[k]);
+            }
+            psortedarr.clear();
+        }
+        arr.clear();
+        sortedarr.clear();
+    }
+}
+
+TYPED_TEST_P(avx2_select, test_small_range)
+{
+    std::vector<int64_t> arrsizes;
+    for (int64_t ii = 0; ii < 1024; ++ii) {
+        arrsizes.push_back(ii);
+    }
+    std::vector<TypeParam> arr;
+    std::vector<TypeParam> sortedarr;
+    std::vector<TypeParam> psortedarr;
+    for (size_t ii = 0; ii < arrsizes.size(); ++ii) {
+        /* Random array */
+        arr = get_uniform_rand_array<TypeParam>(arrsizes[ii], 20, 1);
+        sortedarr = arr;
+        /* Sort with std::sort for comparison */
+        std::sort(sortedarr.begin(), sortedarr.end());
+        for (size_t k = 0; k < arr.size(); ++k) {
+            psortedarr = arr;
+            avx2_qselect<TypeParam>(
+                    psortedarr.data(), k, psortedarr.size());
+            /* index k is correct */
+            ASSERT_EQ(sortedarr[k], psortedarr[k]);
+            /* Check left partition */
+            for (size_t jj = 0; jj < k; jj++) {
+                ASSERT_LE(psortedarr[jj], psortedarr[k]);
+            }
+            /* Check right partition */
+            for (size_t jj = k + 1; jj < arr.size(); jj++) {
+                ASSERT_GE(psortedarr[jj], psortedarr[k]);
+            }
+            psortedarr.clear();
+        }
+        arr.clear();
+        sortedarr.clear();
+    }
+}
+
+REGISTER_TYPED_TEST_SUITE_P(avx2_select, test_random, test_small_range);
+INSTANTIATE_TYPED_TEST_SUITE_P(T, avx2_select, Types);
