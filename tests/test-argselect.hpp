608538dd639b977a0d1fd54bc0b0cb517558bc7d
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2023-07-18 14:53:43 -0700

    More test condition for argselect

--- tests/test-argselect.hpp
+++ tests/test-argselect.hpp
@@ -8,6 +8,27 @@ class avx512argselect : public ::testing::Test {
 };
 TYPED_TEST_SUITE_P(avx512argselect);
 
+
+template <typename T>
+T std_min_element(std::vector<T> arr, std::vector<int64_t> arg, int64_t left, int64_t right)
+{
+    std::vector<int64_t>::iterator res =
+        std::min_element(arg.begin() + left,
+                         arg.begin() + right,
+                         [arr](int64_t a, int64_t b) -> bool {return arr[a] < arr[b];});
+    return arr[*res];
+}
+
+template <typename T>
+T std_max_element(std::vector<T> arr, std::vector<int64_t> arg, int64_t left, int64_t right)
+{
+    std::vector<int64_t>::iterator res =
+        std::max_element(arg.begin() + left,
+                         arg.begin() + right,
+                         [arr](int64_t a, int64_t b) -> bool {return arr[a] > arr[b];});
+    return arr[*res];
+}
+
 TYPED_TEST_P(avx512argselect, test_random)
 {
     if (cpu_has_avx512bw()) {
@@ -21,7 +42,12 @@ TYPED_TEST_P(avx512argselect, test_random)
         for (auto &k : kth) {
             std::vector<int64_t> inx
                     = avx512_argselect<TypeParam>(arr.data(), k, arr.size());
-            EXPECT_EQ(arr[sorted_inx[k]], arr[inx[k]]) << "Failed at index k = " << k;
+            auto true_kth = arr[sorted_inx[k]];
+            EXPECT_EQ(true_kth, arr[inx[k]]) << "Failed at index k = " << k;
+            if (k >= 1)
+                EXPECT_GE(true_kth, std_max_element(arr, inx, 0, k-1));
+            if (k != arrsize-1)
+                EXPECT_LE(true_kth, std_min_element(arr, inx, k+1, arrsize-1));
             EXPECT_UNIQUE(inx)
         }
     }
