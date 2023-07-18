5e95e48454dbbdade35565bc6c65dcf418628740
Matthew Sterrett <matthew.sterrett@intel.com>
2023-07-18 10:51:45 -0700

    Added more tests

--- tests/test-avx2.cpp
+++ tests/test-avx2.cpp
@@ -29,8 +29,99 @@ TYPED_TEST_P(avx2_sort, test_random)
     }
 }
 
+TYPED_TEST_P(avx2_sort, test_reverse)
+{
+    std::vector<int64_t> arrsizes;
+    for (int64_t ii = 0; ii < 1024; ++ii) {
+        arrsizes.push_back((TypeParam)(ii + 1));
+    }
+    std::vector<TypeParam> arr;
+    std::vector<TypeParam> sortedarr;
+    for (size_t ii = 0; ii < arrsizes.size(); ++ii) {
+        /* reverse array */
+        for (int jj = 0; jj < arrsizes[ii]; ++jj) {
+            arr.push_back((TypeParam)(arrsizes[ii] - jj));
+        }
+        sortedarr = arr;
+        /* Sort with std::sort for comparison */
+        std::sort(sortedarr.begin(), sortedarr.end());
+        avx2_qsort<TypeParam>(arr.data(), arr.size());
+        ASSERT_EQ(sortedarr, arr) << "Array size = " << arrsizes[ii];
+        arr.clear();
+        sortedarr.clear();
+    }
+}
+
+TYPED_TEST_P(avx2_sort, test_constant)
+{
+    std::vector<int64_t> arrsizes;
+    for (int64_t ii = 0; ii < 1024; ++ii) {
+        arrsizes.push_back((TypeParam)(ii + 1));
+    }
+    std::vector<TypeParam> arr;
+    std::vector<TypeParam> sortedarr;
+    for (size_t ii = 0; ii < arrsizes.size(); ++ii) {
+        /* constant array */
+        for (int jj = 0; jj < arrsizes[ii]; ++jj) {
+            arr.push_back(ii);
+        }
+        sortedarr = arr;
+        /* Sort with std::sort for comparison */
+        std::sort(sortedarr.begin(), sortedarr.end());
+        avx2_qsort<TypeParam>(arr.data(), arr.size());
+        ASSERT_EQ(sortedarr, arr) << "Array size = " << arrsizes[ii];
+        arr.clear();
+        sortedarr.clear();
+    }
+}
+
+TYPED_TEST_P(avx2_sort, test_small_range)
+{
+    std::vector<int64_t> arrsizes;
+    for (int64_t ii = 0; ii < 1024; ++ii) {
+        arrsizes.push_back((TypeParam)(ii + 1));
+    }
+    std::vector<TypeParam> arr;
+    std::vector<TypeParam> sortedarr;
+    for (size_t ii = 0; ii < arrsizes.size(); ++ii) {
+        arr = get_uniform_rand_array<TypeParam>(arrsizes[ii], 20, 1);
+        sortedarr = arr;
+        /* Sort with std::sort for comparison */
+        std::sort(sortedarr.begin(), sortedarr.end());
+        avx2_qsort<TypeParam>(arr.data(), arr.size());
+        ASSERT_EQ(sortedarr, arr) << "Array size = " << arrsizes[ii];
+        arr.clear();
+        sortedarr.clear();
+    }
+}
+
+TYPED_TEST_P(avx2_sort, test_max_value_at_end_of_array)
+{
+    std::vector<int64_t> arrsizes;
+    for (int64_t ii = 1; ii <= 1024; ++ii) {
+        arrsizes.push_back(ii);
+    }
+    std::vector<TypeParam> arr;
+    std::vector<TypeParam> sortedarr;
+    for (auto &size : arrsizes) {
+        arr = get_uniform_rand_array<TypeParam>(size);
+        if (std::numeric_limits<TypeParam>::has_infinity) {
+            arr[size - 1] = std::numeric_limits<TypeParam>::infinity();
+        }
+        else {
+            arr[size - 1] = std::numeric_limits<TypeParam>::max();
+        }
+        sortedarr = arr;
+        avx2_qsort(arr.data(), arr.size());
+        std::sort(sortedarr.begin(), sortedarr.end());
+        EXPECT_EQ(sortedarr, arr) << "Array size = " << size;
+        arr.clear();
+        sortedarr.clear();
+    }
+}
+
 REGISTER_TYPED_TEST_SUITE_P(avx2_sort,
-                            test_random);
+                            test_random, test_reverse, test_constant, test_small_range, test_max_value_at_end_of_array);
 
 using Types = testing::Types<float, int32_t, uint32_t>;
 INSTANTIATE_TYPED_TEST_SUITE_P(T, avx2_sort, Types);
