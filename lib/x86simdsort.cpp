68938d49fe4c77477aa13fabe5847abf1fd8cfce
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2023-10-12 11:11:14 -0700

    Bug fix in runtime dispatch and variadic args

--- lib/x86simdsort.cpp
+++ lib/x86simdsort.cpp
@@ -7,7 +7,7 @@
 
 static int check_cpu_feature_support(std::string_view cpufeature)
 {
-    const char* disable_avx512 = std::getenv("XSS_DISABLE_AVX512");
+    const char *disable_avx512 = std::getenv("XSS_DISABLE_AVX512");
 
     if ((cpufeature == "avx512_spr") && (!disable_avx512))
 #ifdef __FLT16_MAX__
@@ -100,20 +100,20 @@ dispatch_requested(std::string_view cpurequested,
     }
 
 /* runtime dispatch mechanism */
-#define DISPATCH(func, TYPE, ...) \
+#define DISPATCH(func, TYPE, ISA) \
     DECLARE_INTERNAL_##func(TYPE) static __attribute__((constructor)) void \
     CAT(CAT(resolve_, func), TYPE)(void) \
     { \
         CAT(CAT(internal_, func), TYPE) = &xss::scalar::func<TYPE>; \
         __builtin_cpu_init(); \
-        std::string_view preferred_cpu = find_preferred_cpu({__VA_ARGS__}); \
-        if constexpr (dispatch_requested("avx512", {__VA_ARGS__})) { \
+        std::string_view preferred_cpu = find_preferred_cpu(ISA); \
+        if constexpr (dispatch_requested("avx512", ISA)) { \
             if (preferred_cpu.find("avx512") != std::string_view::npos) { \
                 CAT(CAT(internal_, func), TYPE) = &xss::avx512::func<TYPE>; \
                 return; \
             } \
         } \
-        else if constexpr (dispatch_requested("avx2", {__VA_ARGS__})) { \
+        if constexpr (dispatch_requested("avx2", ISA)) { \
             if (preferred_cpu.find("avx2") != std::string_view::npos) { \
                 CAT(CAT(internal_, func), TYPE) = &xss::avx2::func<TYPE>; \
                 return; \
@@ -121,13 +121,19 @@ dispatch_requested(std::string_view cpurequested,
         } \
     }
 
+#define ISA_LIST(...) \
+    std::initializer_list<std::string_view> \
+    { \
+        __VA_ARGS__ \
+    }
+
 namespace x86simdsort {
 #ifdef __FLT16_MAX__
-DISPATCH(qsort, _Float16, "avx512_spr")
-DISPATCH(qselect, _Float16, "avx512_spr")
-DISPATCH(partial_qsort, _Float16, "avx512_spr")
-DISPATCH(argsort, _Float16, "none")
-DISPATCH(argselect, _Float16, "none")
+DISPATCH(qsort, _Float16, ISA_LIST("avx512_spr"))
+DISPATCH(qselect, _Float16, ISA_LIST("avx512_spr"))
+DISPATCH(partial_qsort, _Float16, ISA_LIST("avx512_spr"))
+DISPATCH(argsort, _Float16, ISA_LIST("none"))
+DISPATCH(argselect, _Float16, ISA_LIST("none"))
 #endif
 
 #define DISPATCH_ALL(func, ISA_16BIT, ISA_32BIT, ISA_64BIT) \
@@ -140,10 +146,25 @@ DISPATCH(argselect, _Float16, "none")
     DISPATCH(func, uint64_t, ISA_64BIT) \
     DISPATCH(func, double, ISA_64BIT)
 
-DISPATCH_ALL(qsort, ("avx512_icl"), ("avx512_skx","avx2"), ("avx512_skx"))
-DISPATCH_ALL(qselect, ("avx512_icl"), ("avx512_skx","avx2"), ("avx512_skx"))
-DISPATCH_ALL(partial_qsort, ("avx512_icl"), ("avx512_skx","avx2"), ("avx512_skx"))
-DISPATCH_ALL(argsort, "none", "avx512_skx", "avx512_skx")
-DISPATCH_ALL(argselect, "none", "avx512_skx", "avx512_skx")
+DISPATCH_ALL(qsort,
+             (ISA_LIST("avx512_icl")),
+             (ISA_LIST("avx512_skx", "avx2")),
+             (ISA_LIST("avx512_skx")))
+DISPATCH_ALL(qselect,
+             (ISA_LIST("avx512_icl")),
+             (ISA_LIST("avx512_skx", "avx2")),
+             (ISA_LIST("avx512_skx")))
+DISPATCH_ALL(partial_qsort,
+             (ISA_LIST("avx512_icl")),
+             (ISA_LIST("avx512_skx", "avx2")),
+             (ISA_LIST("avx512_skx")))
+DISPATCH_ALL(argsort,
+             (ISA_LIST("none")),
+             (ISA_LIST("avx512_skx")),
+             (ISA_LIST("avx512_skx")))
+DISPATCH_ALL(argselect,
+             (ISA_LIST("none")),
+             (ISA_LIST("avx512_skx")),
+             (ISA_LIST("avx512_skx")))
 
 } // namespace x86simdsort
