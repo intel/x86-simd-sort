acd82283b45eb8cff2449359c113296dbb1d5ecd
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2023-11-28 14:16:22 -0800

    Add tests for 32-bit key-value sort

--- tests/test-keyvalue.cpp
+++ tests/test-keyvalue.cpp
@@ -40,28 +40,32 @@ TYPED_TEST_P(simdkvsort, test_kvsort)
             std::vector<T1> key_bckp = key;
             std::vector<T2> val_bckp = val;
             x86simdsort::keyvalue_qsort(key.data(), val.data(), size, hasnan);
-            xss::scalar::keyvalue_qsort(key_bckp.data(), val_bckp.data(), size, hasnan);
+            xss::scalar::keyvalue_qsort(
+                    key_bckp.data(), val_bckp.data(), size, hasnan);
             ASSERT_EQ(key, key_bckp);
-            const bool hasDuplicates = std::adjacent_find(key.begin(), key.end()) != key.end();
-            if (!hasDuplicates) {
-                ASSERT_EQ(val, val_bckp);
-            }
-            key.clear(); val.clear();
-            key_bckp.clear(); val_bckp.clear();
+            const bool hasDuplicates
+                    = std::adjacent_find(key.begin(), key.end()) != key.end();
+            if (!hasDuplicates) { ASSERT_EQ(val, val_bckp); }
+            key.clear();
+            val.clear();
+            key_bckp.clear();
+            val_bckp.clear();
         }
     }
 }
 
 REGISTER_TYPED_TEST_SUITE_P(simdkvsort, test_kvsort);
 
-using QKVSortTestTypes = testing::Types<std::tuple<double, double>,
-                                      std::tuple<double, uint64_t>,
-                                      std::tuple<double, int64_t>,
-                                      std::tuple<uint64_t, double>,
-                                      std::tuple<uint64_t, uint64_t>,
-                                      std::tuple<uint64_t, int64_t>,
-                                      std::tuple<int64_t, double>,
-                                      std::tuple<int64_t, uint64_t>,
-                                      std::tuple<int64_t, int64_t>>;
+#define CREATE_TUPLES(type) \
+    std::tuple<double, type>, std::tuple<uint64_t, type>, \
+            std::tuple<int64_t, type>, std::tuple<float, type>, \
+            std::tuple<uint32_t, type>, std::tuple<int32_t, type>
+
+using QKVSortTestTypes = testing::Types<CREATE_TUPLES(double),
+                                        CREATE_TUPLES(uint64_t),
+                                        CREATE_TUPLES(int64_t),
+                                        CREATE_TUPLES(uint32_t),
+                                        CREATE_TUPLES(int32_t),
+                                        CREATE_TUPLES(float)>;
 
 INSTANTIATE_TYPED_TEST_SUITE_P(xss, simdkvsort, QKVSortTestTypes);
