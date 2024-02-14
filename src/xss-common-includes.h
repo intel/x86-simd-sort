99782e91e95f0561df5c79b115e00f9cf4e6390a
Ian Faust <icfaust@gmail.com>
2024-02-14 10:27:46 +0100

    Update xss-common-includes.h

--- src/xss-common-includes.h
+++ src/xss-common-includes.h
@@ -65,7 +65,9 @@
 #define UNLIKELY(x) (x)
 #endif
 
-#if __GNUC__ >= 8 and !defined(__SANITIZE_ADDRESS__)
+#if defined(__INTEL_COMPILER) and !defined(__SANITIZE_ADDRESS__)
+#define X86_SIMD_SORT_UNROLL_LOOP(num) PRAGMA(unroll(num))
+#elif __GNUC__ >= 8 and !defined(__SANITIZE_ADDRESS__)
 #define X86_SIMD_SORT_UNROLL_LOOP(num) PRAGMA(GCC unroll num)
 #else
 #define X86_SIMD_SORT_UNROLL_LOOP(num)
