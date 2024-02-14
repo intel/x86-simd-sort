e49fe209032bb9b1cf85eb977f0079249f2c19aa
icfaust <icfaust@gmail.com>
2024-02-14 07:28:41 -0800

    move away from docker

--- .github/workflows/build-test-on-docker.sh
+++ .github/workflows/build-test-on-docker.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
 ## Set up environment
-if ! command -v python3 &> /dev/null
-then
-    /opt/python/cp39-cp39/bin/python -mvenv venv
-    source venv/bin/activate
-else
-    apt-add-repository universe
-    apt-get update
-    apt-get install python3-pip -y
-fi
+/opt/python/cp39-cp39/bin/python -mvenv venv
+source venv/bin/activate
 python3 -m pip install meson ninja
-export CXX=$1
-if [[ $# -gt 1 ]]
-then
-    export CXXFLAGS=$2
-fi
+export CXX=g++
 
 ## Install google test from source
 git clone https://github.com/google/googletest.git -b v1.14.0
@@ -27,10 +16,8 @@ make install
 
 ## Install Intel SDE
 curl -o /tmp/sde.tar.xz https://downloadmirror.intel.com/784319/sde-external-9.24.0-2023-07-13-lin.tar.xz
-mkdir /tmp/sde && tar -xf /tmp/sde.tar.xz -C /tmp/sde/
-mv /tmp/sde/* /opt/sde && ln -s /opt/sde/sde /usr/local/bin/sde
-ls /usr/local/bin
-ls /opt/sde
+mkdir /tmp/sde && tar -xvf /tmp/sde.tar.xz -C /tmp/sde/
+mv /tmp/sde/* /opt/sde && ln -s /opt/sde/sde /usr/bin/sde
 
 ## Build x86-simd-sort
 cd /xss
